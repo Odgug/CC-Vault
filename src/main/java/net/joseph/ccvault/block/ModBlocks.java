e0a81f1db5b982152c3dc558a54b48f0dde25f6c
Odgug <urmomafrog@gmail.com>
2023-12-10 15:32:24 +0200

    material to stone + blockdrop

--- src/main/java/net/joseph/ccvault/block/ModBlocks.java
+++ src/main/java/net/joseph/ccvault/block/ModBlocks.java
@@ -1,5 +1,6 @@
 package net.joseph.ccvault.block;
 
+import dan200.computercraft.shared.util.CreativeTabMain;
 import net.joseph.ccvault.CCVault;
 import net.joseph.ccvault.block.custom.VaultReaderBlock;
 import net.joseph.ccvault.item.ModItems;
@@ -13,15 +14,16 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-
+import dan200.computercraft.shared.Registry.*;
 import java.util.function.Supplier;
+
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, CCVault.MOD_ID);
 
     public static final RegistryObject<Block> VAULT_READER_BLOCK = registerBlock("vault_reader",
             () -> new VaultReaderBlock(BlockBehaviour
-                    .Properties.of(Material.METAL).requiresCorrectToolForDrops()),CreativeModeTab.TAB_MISC);
+                    .Properties.of(Material.STONE).strength(0.2f)), CreativeModeTab.TAB_MISC);
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name,block);
         registerBlockItem(name,toReturn,tab);
